60b6897387831bfc449257b28a890889ac9d8b3a
Soumik Majumder <soumikcode02@gmail.com>
2025-10-29 00:22:03 +0530

    feat: added convex for database with schema and add, update, delete functions

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,18 +1,44 @@
+import { api } from "@/convex/_generated/api";
 import useTheme from "@/hooks/useTheme";
+import { useMutation, useQuery } from "convex/react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
-  const { toggleDarkMode } = useTheme();
+  const { isDarkMode, toggleDarkMode } = useTheme();
+  const todos = useQuery(api.todos.getTodos);
+  console.log(todos, "here");
+
+  const addTodo = useMutation(api.todos.addTodo);
+  const clearTodos = useMutation(api.todos.deleteAll);
+  const updateTodo = useMutation(api.todos.updateTodo);
+
+  function execute()
+  {
+    toggleDarkMode();
+    if (todos?.length) {
+      clearTodos();
+    }
+    else
+      addTodo({ text: "This is new todo" });
+
+    // if (todos?.length)
+    // {
+    //   updateTodo({
+    //     id: todos[0]._id,
+    //     text: !isDarkMode? "Hey this is todo" : "This is new todo"
+    //   })
+    // }
+  }
 
   return (
     <View style={styles.container}>
       <Text>Hello World!</Text>
 
-      <TouchableOpacity style={styles.btn} onPress={toggleDarkMode}>
-        <Text>Switch Theme</Text>
+      <TouchableOpacity style={styles.btn} onPress={execute}>
+        <Text>Switch Theme and Manipulate todos</Text>
       </TouchableOpacity>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
