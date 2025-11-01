0f31f1eef65767bb0520705eb1f13045d505697f
Soumik Majumder <soumikcode02@gmail.com>
2025-11-02 01:33:33 +0530

    feat: added delete todo functionality with custom modal for alert

---
+++ assets/styles/delete-alert.styles.ts
@@ -0,0 +1,58 @@
+import { ColorScheme } from "@/hooks/useTheme";
+import { StyleSheet } from "react-native";
+
+export const createDeleteAlertStyles = (colors: ColorScheme) => {
+  const styles = StyleSheet.create({
+    overlay: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: "rgba(0,0,0,0.5)",
+    },
+    container: {
+      width: 300,
+      borderRadius: 20,
+      padding: 20,
+      shadowColor: "#000",
+      shadowOffset: { width: 0, height: 4 },
+      shadowOpacity: 0.2,
+      shadowRadius: 6,
+      elevation: 8,
+    },
+    title: {
+      fontSize: 22,
+      fontWeight: "700",
+      letterSpacing: -1,
+      marginBottom: 10,
+      textAlign: "center",
+      color: colors.text,
+    },
+    subtitle: {
+      fontSize: 17,
+      fontWeight: "500",
+      textAlign: "center",
+      marginBottom: 20,
+      color: colors.textMuted,
+    },
+    actions: {
+      flexDirection: "row",
+      justifyContent: "center",
+      alignItems: "center",
+      gap: 50,
+    },
+    button: {
+      paddingHorizontal: 20,
+      paddingVertical: 8,
+      borderRadius: 5,
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    buttonText: {
+      color: "#ffffff",
+      fontWeight: "600",
+      fontSize: 15,
+    },
+  });
+
+  return styles;
+}
---
+++ components/DeleteAlert.tsx
@@ -0,0 +1,52 @@
+import { createDeleteAlertStyles } from '@/assets/styles/delete-alert.styles';
+import { api } from '@/convex/_generated/api';
+import { Id } from '@/convex/_generated/dataModel';
+import useTheme from '@/hooks/useTheme';
+import { useMutation } from 'convex/react';
+import { LinearGradient } from 'expo-linear-gradient';
+import React from 'react';
+import { Modal, Text, TouchableOpacity, View } from 'react-native';
+
+interface DeleteAlertProps {
+  id: Id<"todos">;
+  isActive: boolean;
+  setIsActive: (v: boolean) => void;
+}
+
+export default function DeleteAlert({ id, isActive, setIsActive }: DeleteAlertProps) {
+  const { colors } = useTheme();
+  const deleteTodo = useMutation(api.todos.deleteTodo);
+  const deleteAlertStyles = createDeleteAlertStyles(colors);
+  
+  async function handleConfirm()
+  {
+    await deleteTodo({ id });
+    setIsActive(false);
+  }
+
+  return (
+    <Modal transparent visible={isActive} animationType="fade">
+      <View style={deleteAlertStyles.overlay}>
+        <LinearGradient colors={colors.gradients.surface} start={{ x: 0, y: 0 }} end={{ x: 1, y: 1 }} style={deleteAlertStyles.container} >
+          <Text style={deleteAlertStyles.title}>Delete this todo</Text>
+          <Text style={deleteAlertStyles.subtitle}>Are you sure about this?</Text>
+
+          <View style={deleteAlertStyles.actions}>
+            <TouchableOpacity activeOpacity={0.8} onPress={() => setIsActive(false)}>
+              <LinearGradient colors={colors.gradients.warning} style={deleteAlertStyles.button} >
+                <Text style={deleteAlertStyles.buttonText}>Cancel</Text>
+              </LinearGradient>
+            </TouchableOpacity>
+
+            <TouchableOpacity activeOpacity={0.8} onPress={handleConfirm}>
+              <LinearGradient colors={colors.gradients.danger} style={deleteAlertStyles.button} >
+                <Text style={deleteAlertStyles.buttonText}>Delete</Text>
+              </LinearGradient>
+            </TouchableOpacity>
+          </View>
+        </LinearGradient>
+      </View>
+
+    </Modal>
+  )
+}
